9f61bbaf1e07cf1695d5958be5fe299506be8351
EC2 Default User <ec2-user@ip-172-31-20-225.us-east-2.compute.internal>
2021-06-14 16:58:32 +0000

    Modify kube run command

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=bishoy/mymlapp:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment --image=$dockerpath mymlapp-con
+kubectl run mymlapp-con1 --image=bishoy/mymlapp:v1.0.0 
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward $dockerpath 8000:80
+kubectl port-forward mymlapp-con1 8000:80
